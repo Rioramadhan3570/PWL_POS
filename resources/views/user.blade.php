6f5ec81dfb0037ac2c33ea97e11db589632c6735
Rioramadhan3570 <rramadhan960@gmail.com>
2024-09-18 08:21:24 +0700

    Praktikum-1

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Data User</title>
-</head>
-<body>
-    <h1>Data User</h1>
-    <table border="2" cellpadding="0" cellspacing="0">
-        <tr>
-            <th>ID</th><th>Nama</th><th>User name</th><th>Password</th><th>ID Level Pengguna</th>
-        </tr>
-        @foreach ($data as $d)
-        <tr>
-            <td>{{ $d->id }}</td><td>{{ $d->nama }}</td><td>{{ $d->user_name }}</td><td>{{ $d->password }}</td><td>{{ $d->id_level_pengguna }}</td>
-        </tr>
-        @endforeach
-    </table>
-</body>
+    <head>
+        <title>Data User</title>
+    </head>
+    <body>
+        <h1>Data User</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+            </tr>
+            @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+            </tr>
+            @endforeach
+        </table>
+    </body>
 </html>
