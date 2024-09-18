2fd83d500d76f1eb274724f9da7cf685de03ce8e
Rioramadhan3570 <rramadhan960@gmail.com>
2024-09-18 09:04:02 +0700

    Praktikum-2.3

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Data User</title>
-</head>
-<body>
-    <h1>Data User</h1>
-    <table border="1">
-        <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $data }}</td>
-        </tr>
-    </table>
-</body>
+    <head>
+        <title>Data User</title>
+    </head>
+    <body>
+        <h1>Data User</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>Jumlah Pengguna</th>
+            </tr>
+            <tr>
+                <td>{{ $data }}</td>
+            </tr>
+        </table>
+    </body>
 </html>
