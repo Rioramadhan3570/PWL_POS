2a6bf53febbaf5db9a1a9d51892f77eb938dc12d
Rioramadhan3570 <rramadhan960@gmail.com>
2024-11-06 11:43:27 +0700

    update KategoriController

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\KategoriModel;
+use Illuminate\Support\Facades\Validator;
 
 class KategoriController extends Controller
 {
@@ -14,8 +16,31 @@ class KategoriController extends Controller
 
     public function store(Request $request)
     {
-        $kategori = KategoriModel::create($request->all());
-        return response()->json($kategori, 201);
+        $validator = Validator::make($request->all(), [
+            'kategori_kode' => 'required|string|min:1|unique:m_kategori,kategori_kode',
+            'kategori_nama' => 'required|string|max:100'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $kategori = KategoriModel::create([
+            'kategori_kode' => $request->kategori_kode,
+            'kategori_nama' => $request->kategori_nama
+        ]);
+
+        if ($kategori) {
+            return response()->json([
+                'success' => true,
+                'kategori' => $kategori,
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Data Gagal Disimpan'
+        ], 409);
     }
 
     public function show(KategoriModel $kategori)
@@ -23,7 +48,7 @@ class KategoriController extends Controller
         return response()->json($kategori);
     }
 
-    public function update(Request $request, KategoriModel $kategori)   
+    public function update(Request $request, KategoriModel $kategori)
     {
         $kategori->update($request->all());
         return response()->json($kategori);
